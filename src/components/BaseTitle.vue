cc1391730cd79b8b6ab82b4f61eb5aeee27e45e7
urlastway <andriy.semenchenko13@gmail.com>
2022-06-01 15:57:42 +0200

    basetitle

---
+++ src/components/BaseTitle.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <h1>BaseTitle</h1>
+  </div>
+</template>
+
+<script>
+export default {
+name:'BaseTitle',
+}
+</script>
+
+<style lang="scss" scoped >
+h1{
+    color: red;
+}
+</style>
